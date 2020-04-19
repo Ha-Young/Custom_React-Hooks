0e7252ecc5e3f4ad499fc55001d7a4fd8cfaea9c
hychoi <hayeong28@naver.com>
2020-04-19 17:10:00 +0900

    useLockScroll

---
+++ useLockScroll/init.js
@@ -0,0 +1,1 @@
+export {useLockScroll as default} from "./useLockScroll";
---
+++ useLockScroll/useLockScroll.js
@@ -0,0 +1,14 @@
+export const useLockScroll = () => {
+    const [initialScroll] = useState(document.body.style.overflow);
+    const [isLocked, setIsLocked] = useState(false);
+    const lockScroll = () => {
+      document.body.style.overflow = "hidden";
+      setIsLocked(true);
+    };
+    const unlockScroll = () => {
+      document.body.style.overflow = initialScroll;
+      setIsLocked(false);
+    };
+    return [isLocked, { lockScroll, unlockScroll }];
+  };
+  
