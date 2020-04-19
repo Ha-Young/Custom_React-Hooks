b40fb790a0b17c04fbb2a2f1dadc9e80f8dac93a
hychoi <hayeong28@naver.com>
2020-04-19 17:06:37 +0900

    useGeolocation

---
+++ useGeolocation/useGeolocation.js
@@ -0,0 +1,16 @@
+export const useGeolocation = () => {
+    const [coords, setCoords] = useState({ lat: "null", long: "" });
+    const [error, setError] = useState("");
+    const onSucces = event => {
+      const {
+        coords: { latitude: lat, longitude: long }
+      } = event;
+      setCoords({ lat, long });
+    };
+    const onFailure = event => setError(event);
+    useEffect(() => {
+      navigator.geolocation.getCurrentPosition(onSucces, onFailure);
+    }, []);
+    return { coords, error };
+  };
+  
